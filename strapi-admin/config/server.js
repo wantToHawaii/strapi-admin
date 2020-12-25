1305d9ddc2f0ad3409f50d0f99cc8a4639af94eb
Eugene K. (wantToHawaii) <ekaryakin@bekitzur.com>
2020-12-26 00:56:04 +0300

    Initial commit

---
+++ strapi-admin/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bd8e2f80521bb10197954a889074d448'),
+    },
+  },
+});
---
+++ strapi-admin/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '59edcfd1-0053-4b0f-804f-15a7e38f3d66'
+};
